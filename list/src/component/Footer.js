3bbdd49bec2797319daec7c8171b42a5b0f67f9c
Mustafa Gençtürk <mustafailetisim1@gmail.com>
2022-06-30 22:45:47 +0300

    30.06.2022
    Details...

--- list/src/component/Footer.js
+++ list/src/component/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="card-body">
                 <blockquote className="blockquote mb-0">
                 <p>Watch, explore, record, share. All we need is some fun.</p>
-                <footer className="blockquote-footer">Someone who don't want to share name<cite title="Source Title"></cite></footer>
+                <footer className="blockquote-footer">Someone who don't want to name<cite title="Source Title"></cite></footer>
                 </blockquote>
             </div>
             </div>  
